0a4cd4a2d9189d06379463ccc9b14cebdf8626fe
peninak <pkessler@wesleyan.edu>
2016-03-14 10:10:11 -0400

    saving more files

---
+++ js/tour.js
@@ -0,0 +1,23 @@
+var tourImages = ["../images/tour-images/sxsw.jpg","../images/tour-images/winter-tour.jpg", "../images/tour-images/fall-tour.jpg", "../images/tour-images/london-tour.jpg", 
+"../images/tour-images/dublin-tour.jpg", "../images/tour-images/june-tour.jpg"];
+
+$(document).ready(function() {
+	insertPix();
+});
+
+function insertPix() {
+	for(var i=0; i<tourImages.length; i++) {
+		$('.tour-slideshow').append('<img src=' + tourImages[i] + 'class="tour-image"/>');
+	}
+}
+
+$('.tour-slideshow').on('click', function() {
+	$(this).height('25%');
+	$(this).width('70%');
+	$('.tour-image').css('visibility', 'hidden');
+	$(this).css({
+		'visibility': 'visible',
+		'vertical-align': 'middle',
+	});
+	$('body').css('background-color', 'rgba(0, 0, 0, 0.85)');
+});
--- js/video.js
+++ js/video.js
@@ -7,17 +7,34 @@ $(document).ready(function() {
 	}, 5000);
 
 	$('.body').animate({
-		width: '49%', 
+		width: '49.5%', 
 		height: '450px',
+		margin: 0,
 	}, 5000);
 
 	$('.mainFrame').animate({
 		width: $(window).width(), 
+		margin: 0,
 	}, 5000);
 
-	$('a').css({'color': '#f5efd0'});
-	// $('a').addClass('.vid');
-	// $('bttn').width($('input').width());
-	// $('bttn').height($('input').height());
-	
+	// $('a').animate({
+	// 	color: '#f5efd0'
+	// }, 5000);
 });
+
+// $(window).on('scroll', function(){
+// 	var scrollHeight = $(document).height();
+// 	var scrollPosition = $(window).height() + $(window).scrollTop();
+// 	if ((scrollHeight - scrollPosition) / scrollHeight === 0) {
+// 		$('a').css({
+// 			'color': '#000',
+// 			'font-weight': '400'
+// 		});
+// 	}
+// 	else {
+// 		$('a').css({
+// 			'color': '#f5efd0',
+// 			'font-weight': '100'
+// 		});
+// 	}
+// });
