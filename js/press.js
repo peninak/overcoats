f3bafce735e34e39551e5f08859ec3c9cadc876f
penina kessler <pkessler@wesleyan.edu>
2016-03-15 14:30:23 -0400

    JK JK THIS

--- js/press.js
+++ js/press.js
@@ -1,19 +1,11 @@
-<<<<<<< HEAD
-var pressImagesAdded =  ['../images/press-images/tigress.jpg', '../images/press-images/the-deli.jpg', '../images/press-images/crack-in-the-road.jpg', '../images/press-images/the-blue-walrus.png', '../images/press-images/la-music-blog.jpg', '../images/press-images/killing-moon.png',
-=======
+
 var pressImagesAdded =  ['../images/press-images/the-deli.jpg', '../images/press-images/crack-in-the-road.jpg', '../images/press-images/the-blue-walrus.png', '../images/press-images/la-music-blog.jpg', '../images/press-images/killing-moon.png',
->>>>>>> origin/master
 '../images/press-images/the-405.png', '../images/press-images/next-2-shine.png', 
 '../images/press-images/method.png', '../images/press-images/this-is-our-jam.jpeg', 
 '../images/press-images/next-2-shine.png', '../images/press-images/aural-wes.png',
 '../images/press-images/hilly-dilly.png'];
-<<<<<<< HEAD
 var pressImagesDisplayed = ['../images/press-images/honey-pie.jpg', '../images/press-images/npr.jpeg', '../images/press-images/line-of-best.png', '../images/press-images/bullett.jpg', 
 '../images/press-images/clash.jpg', '../images/press-images/axs.png',];
-=======
-var pressImagesDisplayed = ['../images/press-images/npr.jpeg', '../images/press-images/line-of-best.png', '../images/press-images/bullett.jpg', 
-'../images/press-images/clash.jpg', '../images/press-images/axs.png', '../images/press-images/tigress.jpg',];
->>>>>>> origin/master
 var pressLinks = ['http://www.npr.org/sections/world-cafe/2016/03/08/469053101/heavy-rotation-10-songs-public-radio-cant-stop-playing', ''];
 var i=0; 
 var display=0;
@@ -49,15 +41,12 @@ function addContent() {
 	}
 	};
 
-<<<<<<< HEAD
 // $('.boxes').on('hover', function() {
 // 		$(this).addClass('.overlay');
 // 	});
-=======
 $('.boxes').on('hover', function() {
 		$(this).addClass('.overlay');
 	});
->>>>>>> origin/master
 
 
 $(document).ready(function() {
