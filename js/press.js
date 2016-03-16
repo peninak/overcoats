94cf642227e38fa2ad99b6d7df67b690cef5a82f
penina kessler <pkessler@wesleyan.edu>
2016-03-15 21:12:33 -0400

    with functioning js and CSS for press page. also fixed background image bug

--- js/press.js
+++ js/press.js
@@ -6,9 +6,17 @@ var pressImagesAdded =  ['../images/press-images/the-deli.jpg', '../images/press
 '../images/press-images/hilly-dilly.png'];
 var pressImagesDisplayed = ['../images/press-images/honey-pie.jpg', '../images/press-images/npr.jpeg', '../images/press-images/line-of-best.png', '../images/press-images/bullett.jpg', 
 '../images/press-images/clash.jpg', '../images/press-images/axs.png',];
-var pressLinks = ['http://www.npr.org/sections/world-cafe/2016/03/08/469053101/heavy-rotation-10-songs-public-radio-cant-stop-playing', ''];
+var pressLinks = ['http://www.thewildhoneypie.com/get-to-know-the-sweet-harmonies-and-unique-rhythms-of-overcoats/',
+'http://www.npr.org/sections/world-cafe/2016/03/08/469053101/heavy-rotation-10-songs-public-radio-cant-stop-playing', 
+'http://www.thelineofbestfit.com/new-music/discovery/overcoats-nighttime-hunger', 'http://www.clashmusic.com/news/premiere-overcoats-nighttime-hunger', 
+'http://bullettmedia.com/article/nyc-duo-overcoats-are-afraid-of-the-dark-on-nighttime-hunger/','https://www.axs.com/news/overcoats-to-perform-in-nyc-ahead-of-sxsw-appearance-77384'];
+var headlines = ['GET TO KNOW THE SWEET HARMONIES AND UNIQUE RHYTHMS OF OVERCOATS', "Heavy Rotation: 10 Songs Public Radio Can't Stop Playing", 
+'Overcoats explore insomnia with hypnotic electronic track “Nighttime Hunger”', "Premiere: Overcoats - 'Nighttime Hunger'", "NYC Duo Overcoats Are Afraid of the Dark on 'Nighttime Hunger'",
+'Overcoats to perform in NYC ahead of SXSW appearance'];
 var i=0; 
 var display=0;
+var pressLinksIndex=0;
+var headlinesIndex=0;
 
 function loadMore() {
 		$(window).on('scroll', function () {
@@ -16,7 +24,7 @@ function loadMore() {
 			var scrollPosition = $(window).height() + $(window).scrollTop();
 			if ((scrollHeight - scrollPosition) / scrollHeight === 0) {
 				while(i<pressImagesAdded.length) {
-					$('.boxes').append('<div class="pressBoxAdded"></div>');
+					$('.boxes').append('<div class="pressBoxAdded"><div class="overlay"></div></div>');
 					$('.pressBoxAdded').eq(i).css({
 						'background-image' : 'url(' + pressImagesAdded[i] + ')',
 						'background-size' : '100% 100%',
@@ -31,23 +39,18 @@ function loadMore() {
 
 function addContent() {
 	while(display<pressImagesDisplayed.length) {
-		$('.boxes').append('<div class="pressBox"></div>');
+		$('.boxes').append('<div class="pressBox"><div class="overlay"></div></div>');
 		$('.pressBox').eq(display).css({
 			'background-image': 'url(' + pressImagesDisplayed[display] + ')', 
 			'background-size': '100% 100%', 
 			'background-repeat': 'no-repeat',
 		});
+		$('.pressBox .overlay').eq(display).html('<a class="press" href=' + pressLinks[pressLinksIndex] + '' + 'target="blank">' + headlines[headlinesIndex] + '</a>');
 		display++;
+		pressLinksIndex++;
+		headlinesIndex++;
 	}
 	};
-
-// $('.boxes').on('hover', function() {
-// 		$(this).addClass('.overlay');
-// 	});
-$('.boxes').on('hover', function() {
-		$(this).addClass('.overlay');
-	});
-
 
 $(document).ready(function() {
 	addContent();
